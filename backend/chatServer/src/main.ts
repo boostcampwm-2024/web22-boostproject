789e919310687e55f735ff482dd38c0039f2a87d
hoeeeeeh <hoeeeeeh@naver.com>
2024-11-25 14:57:00 +0900

    feat: redis adapter 로 수정

--- backend/chatServer/src/main.ts
+++ backend/chatServer/src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { Cluster } from 'ioredis';
+import { ServerOptions } from 'socket.io';
 
 async function bootstrap() {
   // app.enableCors(); // CORS 설정 (웹 클라이언트와의 연결을 허용)
